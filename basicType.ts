06e203fac67b5bada06ba302f5c850878201223b
islam37 <islamuddin3725@gmail.com>
2026-05-07 17:11:10 +0600

    inference

--- basicType.ts
+++ basicType.ts
@@ -1,3 +1,8 @@
 let firstName: string ="rahim"
 let age :number =25
 let isMarried :boolean = false
+
+//type inference 
+//Rule of thumb: If the value is obvious, let TypeScript infer. Write types explicitly when it's not obvious (function params, return values, etc.)
+let city= "dhaka"
+let score =95
